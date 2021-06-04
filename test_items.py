44ac263a65f37fba3777c4cadd838224470596bd
KingDitch <chizh21@yandex.ru>
2021-06-05 00:53:41 +0300

    Final

--- test_items.py
+++ test_items.py
@@ -5,5 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_page_language(browser):
     browser.get(link)
     button_search = browser.find_element_by_css_selector("button.btn-add-to-basket")
-    assert button_search
+    assert button_search, "Button is not found"
     time.sleep(1)
